4ffb75b9ef509f1a8690edbbc8da788943da9394
Hendrik de Lange <hpjdelange@gmail.com>
2026-07-15 11:55:12 +0200

    Add Hilt dependency injection foundation

---
+++ app/src/main/java/za/hendrikdelange/mycompletefitnesstracker/MyCompleteFitnessTrackerApplication.kt
@@ -0,0 +1,7 @@
+package za.hendrikdelange.mycompletefitnesstracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyCompleteFitnessTrackerApplication : Application()
